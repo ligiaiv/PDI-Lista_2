3ee458017524f7861ae5471283ab6329578ce6c7
Lígia <ligia.venturott@gmail.com>
2019-06-20 20:12:43 -0300

    ex3

--- ex3.py
+++ ex3.py
@@ -51,29 +51,43 @@ def erosao(im,A,B):
 #im = padded image
 #A = original object, no padding
 def dilatacao(im,A,B):
-	new_A = np.ndarray(shape = (2,0),dtype = int)
+	# print('A',A.shape)
+	# print('B',B.shape)
+	# print('im',im.shape)
+	# print(A)
+
+	# new_A = np.ndarray(shape = (2,0),dtype = int)
+	new_A = A.copy()
 	padded = pad(im,B)
 	l = B.shape[0]//2
 	h = B.shape[1]//2
 	for pixel in A.T:
 		x = pixel[0]+l
 		y = pixel[1]+h
-
+		# print('pixel',pixel)
 
 		partial = padded[x-l:x+l+1,y-h:y+h+1]/255
 		if (bool(partial[B.astype(bool)].max())):
-			new_A = np.hstack([new_A,pixel[:,None]])
-	new_white = np.concatenate((new_A, white), axis=1)
-	dilatado = np.zeros(im.shape)
-
-	dilatado[(new_white[0],new_white[1])] = 255
+			# print('.',np.invert(np.all(new_A.T==pixel,axis = 1)).sum())
+			# print(np.invert(np.all(new_A.T==pixel,axis = 1)).shape)
+			# print(new_A.shape)
+			new_A = new_A[:,np.invert(np.all(new_A.T==pixel,axis = 1)).T]
+			# print('.',np.where(np.bitwise_and(new_A[0]==x,new_A[1==y])))
+			# new_A = np.delete(new_A, np.argwhere(new_A.T==pixel))
+			# new_A = np.hstack([new_A,pixel[:,None]])
+	# new_white = np.concatenate((new_A, white), axis=1)
+	dilatado = np.ones(im.shape)*255
+	# print(new_A.shape)
+	dilatado[(new_A[0],new_A[1])] = 0
 
 	return new_A,dilatado
 
 def abertura(im,B):
 	white = np.vstack(np.where(im == 255))
-	black = np.vstack(np.where(im == 0))
 	_,im2 = erosao(im,white,B)
+	# print('im2:',im2)
+	black = np.vstack(np.where(im2 == 0))
+	# print(im2.shape)
 	return dilatacao(im2,black,B)
 
 # def dilatacao(im,B):
@@ -106,6 +120,11 @@ white = np.vstack(np.where(im == 255))
 black = np.vstack(np.where(im == 0))
 
 mask = np.array([[0,1,0],[1,1,1],[0,1,0]])
+mask = np.ones((3,3))
+# im = np.array([[1,0,0,0,0],[0,1,1,0,0],[0,1,1,0,0],[0,1,1,1,0],[0,1,1,1,0]])
+# im = np.vstack([im,np.ones((5,5))])
+# im = im*255
+# cv2.imwrite("imagens/ex3/im_livro.jpg",im.astype(np.uint8))
 
 # padded = pad(im,mask)
 
@@ -118,22 +137,60 @@ mask = np.array([[0,1,0],[1,1,1],[0,1,0]])
 # dilatado = np.zeros(im.shape)
 
 # dilatado[(new_white[0],new_white[1])] = 255
-_,aberto = abertura(im,mask) 
-aberto_lib = ndimage.binary_opening(im,structure = mask)
-print('ABERTURA meu ',(aberto==0).astype(int).sum(),' lib ',(aberto_lib==0).astype(int).sum())
-
-_,dilatado = dilatacao(im,black,mask) 
-dilatado_lib = ndimage.binary_dilation(im,structure = mask)
-print('DILATAÇÃO meu ',(dilatado==0).astype(int).sum(),' lib ',(dilatado_lib==0).astype(int).sum())
 
 
-_,erosado = erosao(im,white,mask) 
-erosado_lib = ndimage.binary_erosion(im,structure = mask)
-print('EROSAO meu ',(erosado==0).astype(int).sum(),' lib ',(erosado_lib==0).astype(int).sum())
+
+# _,aberto = abertura(im,mask) 
+# aberto_lib = ndimage.binary_opening(im,structure = mask)
+# print('ABERTURA meu ',(aberto==0).astype(int).sum(),' lib ',(aberto_lib==0).astype(int).sum())
+
+# _,dilatado = dilatacao(im,black,mask) 
+# dilatado_lib = ndimage.binary_dilation(im,structure = mask)
+# print('DILATAÇÃO meu ',(dilatado==0).astype(int).sum(),' lib ',(dilatado_lib==0).astype(int).sum())
+
+
+# _,erosado = erosao(im,white,mask) 
+# erosado_lib = ndimage.binary_erosion(im,structure = mask)
+# print('EROSAO meu ',(erosado==0).astype(int).sum(),' lib ',(erosado_lib==0).astype(int).sum())
+
+
+
 # _,aberto = abertura(im,mask)
 # cv2.imwrite("imagens/ex3/dilata.jpg",aberto.astype(np.uint8))
 # cv2.imwrite("imagens/ex3/aberto.jpg",aberto.astype(np.uint8))
+# teste1 = np.array([[0,1,0],[0,1,0],[1,1,1],[1,1,1],[1,1,1]])*255
+# print(teste1)
+# _,teste1_aberto = abertura(teste1,mask)
+# cv2.imwrite("imagens/ex3/aberto_teste1.jpg",(teste1_aberto*255).astype(np.uint8))
+# print('aberto_teste1',teste1_aberto)
+# quit()
+novo = im
+K_max = 0
+while (novo == 255).sum() >0:
+	K_max+=1
+	velho = novo
+	white = np.vstack(np.where(velho == 255))
+	_,novo = erosao(velho,white,mask) 
+	print((novo==255).sum())
+print('K_max: ',K_max)
+total = np.zeros(im.shape).astype(int)
+for K in range(0,K_max):
+	novo = im
+	print("- - - k = ",K)
+	for k in range(1,K+1):
+		print(k,'/',K) 
+		velho = novo
+		white = np.vstack(np.where(velho == 255))
+		_,novo = erosao(velho,white,mask) 
+	_,aberto = abertura(novo,mask)
+	# cv2.imwrite("imagens/ex3/novo_"+str(K)+".jpg",(novo).astype(np.uint8))
+	# cv2.imwrite("imagens/ex3/aberto_"+str(K)+".jpg",(aberto).astype(np.uint8))
 
+	SkA = novo - aberto
+	SkA[SkA<0]=0
+	total = np.bitwise_or(total,(SkA/255).astype(int))
 end = time.time()
+cv2.imwrite("imagens/ex3/esqueleto.jpg",(total*255).astype(np.uint8))
+# cv2.imwrite("imagens/ex3/novo.jpg",(novo).astype(np.uint8))
 
 print('Tempo de Execução: ', end-start)
